f5cf843c825b212aba4dcbd7f62b4cb78e044b04
Risha <rishallen@gmail.com>
2016-03-30 22:46:06 -0700

    rainbow hash

---
+++ practice_hash.rb
@@ -0,0 +1,39 @@
+#Let's create a rainbow of colors as an array of hashes, where each hash contains the name of the color (key) and the RGB values (value).
+
+rainbow = [
+  :red => {
+    r: 255,
+    g: 0,
+    b: 0
+  }
+  :orange => {
+    r: 255,
+    g: 125,
+    b: 0
+  }
+
+  :yellow => {
+    r: 255,
+    g: 255,
+    b: 0
+  }
+
+  :green => {
+    r: 0,
+    g: 255,
+    b: 0
+  }
+
+  :blue => {
+    r: 0,
+    g: 0,
+    b: 255
+  }
+
+  :purple => {
+    r: 125,
+    g: 0,
+    b: 255
+  }
+
+]
